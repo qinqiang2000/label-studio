dc24ebb0d90f9b815a657bce8584ac607dd191c8
qinqiang2000 <qinqiang2000@qq.com>
2025-06-29 02:09:21 +0800

    删除音视频代码，加速前端加载速度

--- web/libs/datamanager/src/components/DataGroups/index.js
+++ web/libs/datamanager/src/components/DataGroups/index.js
@@ -1,6 +1,2 @@
-export {
-  AudioDataGroup as Audio,
-  AudioDataGroup as AudioPlus,
-} from "./AudioDataGroup";
 export { ImageDataGroup as Image } from "./ImageDataGroup";
 export { TextDataGroup as String, TextDataGroup } from "./TextDataGroup";
